777c98d185e25aa48af5724cef4bcb7fa4721f60
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-25 12:02:18 -0600

    Add artist and functional test

--- spec/features/songs/index_spec.rb
+++ spec/features/songs/index_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe "Songs Index" do
     it "I see all songs and their title and playcount" do
     #each it block is a test
     #use as many describe blocks as you want, but once you are in an it block you can not use describe inside the it block.
-      song_1 = Song.create(title: "Hit Me Baby One More Time", length: 345, playcount: 100000000)
-      song_2 = Song.create(title: "What Is Love", length: 543, playcount: 2)
+      artist_1 = Artist.create!(name: "Britney Spears")
+      artist_2 = Artist.create!(name: "Haddaway")
+
+      song_1 = artist_1.songs.create!(title: "Hit Me Baby One More Time", length: 345, playcount: 100000000)
+      #You can do it either way. Both artist_1.songs.create or Song.create, but the second one you need to include the artist id.
+      song_2 = Song.create!(title: "What Is Love", length: 543, playcount: 2, artist_id: artist_2.id)
 
       visit "/songs"
 
